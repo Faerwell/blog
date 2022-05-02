c2cdc9fc11ecf8a55d404469d63d58049bd2bcf2
Faerwell <nickmisharin@gmail.com>
2022-05-02 17:33:56 +0500

    add render_template

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "home page"\
+    return render_template('index.html', title='Blog Home Page', head_text='Главная страничка')
 
-@app.route("/about")
+@app.route('/about')
 def about():
-    return "about page"
+    return render_template('about.html', title='About Page', head_text='О сайте')
 
 if __name__ == "__main__":
     app.run(debug=True)
