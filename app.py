8cd4c6c504095d7099f330186171e572626619fb
Faerwell <nickmisharin@gmail.com>
2022-05-02 17:42:21 +0500

    add base template and static menu

--- app.py
+++ app.py
@@ -2,14 +2,16 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-
+menu = ['Главная', 'Категории', 'О сайте']
 @app.route('/')
 def index():
-    return render_template('index.html', title='Blog Home Page', head_text='Главная страничка')
+    return render_template('index.html', title='Blog Home Page',
+                           head_text='Главная страничка',
+                           menu = menu)
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='About Page', head_text='О сайте')
+    return render_template('about.html', title='About Page', head_text='О сайте', menu = menu)
 
 if __name__ == "__main__":
     app.run(debug=True)
