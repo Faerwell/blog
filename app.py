a2746da6decc4d7f651bc871f1be3cbf4ebaf5a6
Faerwell <nickmisharin@gmail.com>
2022-05-03 08:55:13 +0500

    add url_for, pico css framework, change menu global variable

--- app.py
+++ app.py
@@ -1,17 +1,33 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
-menu = ['Главная', 'Категории', 'О сайте']
+menu = [{'name': 'Главная', 'url': 'index'},
+        {'name': 'О сайте', 'url': 'about'}]
+brand = 'Faerwell'
+
+
 @app.route('/')
+@app.route("/index")
 def index():
-    return render_template('index.html', title='Blog Home Page',
-                           head_text='Главная страничка',
-                           menu = menu)
+    return render_template('index.html', title = 'Blog Home Page', head_text = 'Главная страничка', menu = menu,
+                           brand = brand)
+
 
 @app.route('/about')
 def about():
-    return render_template('about.html', title='About Page', head_text='О сайте', menu = menu)
+    return render_template('about.html', title = 'About Page', head_text = 'О сайте', menu = menu, brand = brand)
+
+
+@app.route('/profile/<username>')
+def profile(username):
+    return f'Пользователь: {username}'
+
+
+with app.test_request_context():
+    print(url_for('index'))
+    print(url_for('about'))
+    print(url_for('profile', username='Faerwell'))
+
 
 if __name__ == "__main__":
     app.run(debug=True)
