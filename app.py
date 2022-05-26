ebc2111f7a5703fcac452800a6e1797d9958af74
Faerwell <nickmisharin@gmail.com>
2022-05-26 19:06:14 +0500

    add model Post and SQLAlchemy

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
 from config import Config
 from posts.blueprint import posts
+
 
 
 app = Flask(__name__)
@@ -8,5 +11,5 @@ app.config.from_object(Config)
 
 app.register_blueprint(posts, url_prefix='/blog')
 
-
+db = SQLAlchemy(app)
 
--- config.py
+++ config.py
@@ -1,5 +1,12 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__name__))
+
+
 class Config:
     DEBUG = True
     menu = [{'name': 'Блог', 'url': 'posts.posts_list'},
             {'name': 'О сайте', 'url': 'about'}]
     brand = 'Faerwell'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(BASE_DIR, 'database.db')
+    SQLALCHEMY_TRACK_MODIFICATION = False
